55b3daab69cf3196d277722c764dbf62d2dfa2ae
Jagger85 <andresfonsortega@yahoo.es>
2024-12-15 04:51:12 +0700

    Feat(messageType): Created constants for websocket messages types

---
+++ src/constants/messageType.js
@@ -0,0 +1,8 @@
+export const MESSAGE_TYPE = Object.freeze({
+    LOGIN: 'login',
+    LOGOUT: 'logout',
+    OPERATION_STATUS: 'operation_status',
+    OPERATION_PROGRESS: 'operation_progress',
+    INFO: 'info',
+    ERROR: 'error',
+})
