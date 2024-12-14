af6a07d6cfd5f7b8c78f84bc19b8a9ba429f7831
Jagger85 <andresfonsortega@yahoo.es>
2024-12-15 04:49:10 +0700

    Feat(useApi): Created custom hook - Created functions to retrieve the base URL and the websocket URL

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,10 +1,22 @@
 const getBaseUrl = () => {
+  if (import.meta.env.PROD) {
+    return import.meta.env.VITE_API_URL
+  }
 
-    if (import.meta.env.PROD) {
-      return import.meta.env.VITE_API_URL
-    }
-    
-    return `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/api`
+  return `http://${import.meta.env.VITE_BACKEND_HOST}:${
+    import.meta.env.VITE_BACKEND_PORT
+  }/api`
 }
-  
+
+const getWebsocketUrl = () => {
+  if (import.meta.env.PROD) {
+    return import.meta.env.WEB_SOCKET_URL
+  }
+
+  return `ws://${import.meta.env.VITE_BACKEND_HOST}:${
+    import.meta.env.VITE_BACKEND_PORT
+  }/echo`
+}
+
+export const WEB_SOCKET_URL = getWebsocketUrl()
 export const API_BASE_URL = getBaseUrl()
