5912bd850b4b0ea0778d511bfb6909b892d1ea34
Jagger85 <andresfonsortega@yahoo.es>
2025-04-24 11:14:37 +0200

    Chore: Removed production url's and bearer token auth functionality

--- src/hooks/useApi.js
+++ src/hooks/useApi.js
@@ -1,20 +1,8 @@
 const getBaseUrl = () => {
-  if (import.meta.env.PROD) {
-    console.log('Production environment detected');
-    console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
-    // In production, API requests will be proxied through Nginx
-    return '/api'  // Remove the environment variable and use a fixed path
-  }
-
-  console.log('Development environment detected');
   return `http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}`
 }
 
 const getWebsocketUrl = () => {
-  if (import.meta.env.PROD) {
-    return `ws://${window.location.host}/socket.io`  // Use window.location.host for WebSocket
-  }
-
   return `ws://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/echo`
 }
 
@@ -24,8 +12,7 @@ export const API_BASE_URL = getBaseUrl()
 export const sendYoutubeUrl = async (url, operationType, prompt, sendMessage, userId) => {
   try {
     const apiUrl = `${API_BASE_URL}/api/speecher`
-    console.log('API URL:', apiUrl)
-
+    
     // Create base query params
     const queryParams = new URLSearchParams({
       video_url: url,
@@ -41,7 +28,6 @@ export const sendYoutubeUrl = async (url, operationType, prompt, sendMessage, us
     const response = await fetch(`${apiUrl}?${queryParams}`, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTczNzEyODEyMywianRpIjoiZGZmMjI4YzctMDU5Ni00ZTNlLWEzMGMtMzc0ZjA3MDM5MzQyIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6ImJydWNlIiwibmJmIjoxNzM3MTI4MTIzLCJjc3JmIjoiYmE1Y2U2NjMtODkwMi00YzU1LWI3NzgtMzIyYWVlZGEwOTQ2In0.B8_vlOEnpZDnyo-ozUJq_GGK_S7JJ0wngYkslwLQ3Vs`
       },
     })
     if (!response.ok) {
